28e6ecc5b5fbb2d2fa7b4845a4c7548047b5774f
MichaelBurak <22567541+MichaelBurak@users.noreply.github.com>
2021-11-01 21:06:47 -0600

    init comimt

---
+++ vars/buildImage.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy
+
+
+def call() {
+    echo "Building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')])
+            {
+                sh "docker build -t michaelburak/priv-test:jma-2.0 ."
+                sh "echo $PASS | docker login -u $USER --password-stdin"
+                sh "docker push michaelburak/priv-test:jma-2.0"
+            }
+}
